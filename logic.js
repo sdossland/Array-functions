57ac7b7bd8df6a1331eb1c415090f0fe6ee08763
Sophia Dossland <sophia.dossland@gmail.com>
2017-02-28 15:27:46 -0800

    first half of array functions

--- logic.js
+++ logic.js
@@ -11,7 +11,39 @@ const inventors = [
   {firstName: 'Max', lastName: 'Planck', year: 1858, passed: 1947}
 ];
 
-const people = [
-  'Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul'
-];
+// const people = [
+//   'Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul'
+// ];
+
+//filter the inventors by those born in the 1500s
+var fifteenHundredsBirth = inventors.filter(function(inventor) {
+      return inventor.year >= 1500 && inventor.year < 1600;
+});
+// console.table(fifteenHundredsBirth); //worked!
+
+//provide an array of the inventors first and last names
+const arrayOfInventors = inventors.map(function(inventor) {
+  return `${inventor.firstName} ${inventor.lastName}`
+});
+// console.log(arrayOfInventors); //worked!
+
+//Sort the inventors by birthdate, oldest to youngest
+const oldestToYoungest = inventors.sort(function(a, b) {
+  return a.year - b.year;
+});
+// console.table(oldestToYoungest); //worked
+
+//How many years did all of the inventors live
+const totalYears = inventors.reduce(function(total, inventor) {
+  return total + (inventor.passed - inventor.year);
+}, 0);
+// console.log(totalYears); //worked!
+
+//Sort the inventors by years lived, greatest to least
+const yearsLived = inventors.sort(function(a, b) {
+  const aYearsLived = a.passed - a.year;
+  const bYearsLived = b.passed - b.year;
+  return bYearsLived - aYearsLived;
+});
+// console.log(yearsLived); //worked!
 
