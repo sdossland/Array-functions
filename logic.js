fec3253b18262edebfbbed6b37a21d753c9c9e06
Sophia Dossland <sophia.dossland@gmail.com>
2017-02-28 14:39:01 -0800

    initial commit

---
+++ logic.js
@@ -0,0 +1,17 @@
+/**
+ * Created by sophia on 2/28/17.
+ */
+const inventors = [
+  {firstName: 'Albert', lastName: 'Einstein', year: 1879, passed: 1955},
+  {firstName: 'Isaac', lastName: 'Newton', year: 1643, passed: 1727},
+  {firstName: 'Galileo', lastName: 'Galilei', year: 1564, passed: 1642},
+  {firstName: 'Marie', lastName: 'Curie', year: 1867, passed: 1934},
+  {firstName: 'Johannes', lastName: 'Kepler', year: 1571, passed: 1630},
+  {firstName: 'Nicolaus', lastName: 'Copernicus', year: 1473, passed: 1543},
+  {firstName: 'Max', lastName: 'Planck', year: 1858, passed: 1947}
+];
+
+const people = [
+  'Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul'
+];
+
